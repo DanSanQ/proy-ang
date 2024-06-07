2b69d13edd50c13e5e525a38a78022c2165cbd0f
Daniel <dan.san.q.gt@gmail.com>
2024-06-07 04:00:48 -0400

    Primer commit: Subiendo proyecto Angular a GitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SliderComponent } from './slider/slider.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SliderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
